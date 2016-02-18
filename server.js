758cfa30882136e3bd08fba242b96f41cfd674e5
Vaibhav Raut <v.u.raut10@gmail.com>
2016-02-18 09:53:22 +0530

    Setup basic server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var PORT = process.env.PORT || 3000;
+// create app
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(PORT, function(){
+	console.log('Server started');
+});

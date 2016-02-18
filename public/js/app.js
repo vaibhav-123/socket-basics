a416ff9e0b597fdeeae056b94375731b23578b58
Vaibhav Raut <v.u.raut10@gmail.com>
2016-02-18 13:50:46 +0530

    Added Add front end form

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,6 @@
+
+$(document).ready(function() {
+    
 var socket = io();
 
 socket.on('connect', function(){
@@ -7,4 +10,20 @@ socket.on('connect', function(){
 socket.on('message', function(message){
 	console.log('New message:');
 	console.log(message.text);
+});
+
+// Handle submitting of new message
+$('#message-form').on('submit', function(event){
+	
+	event.preventDefault();
+	
+	var $message = $('#message-form').find('input[name=message]');
+	
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('');
+});
+
 });
